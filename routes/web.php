1eefef980ec0483fcdf274d7eded59e7c81b0a18
ihzarizkyk <ihzarizky30@gmail.com>
2022-03-12 21:55:09 +0700

    create Controller Auth, Dashboard and Model Employee, Profile

--- routes/web.php
+++ routes/web.php
@@ -2,33 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    return view('index');
 });
 
-Route::get('/home', function(){
-    return view('home');
+Route::get('/auth/login', function(){
+    return view('auth.login');
 });
 
-Route::get('/login', function(){
-    return view('login');
-});
-
-Route::get('/register', function(){
-    return view('register');
+Route::get('/auth/register', function(){
+    return view('auth.register');
 });
 
 Route::get('/dashboard', function(){
-    return view('dashboard');
+    return view('dashboard.index');
 });
