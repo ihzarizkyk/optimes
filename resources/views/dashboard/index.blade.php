fcba480c6904a030f7448d8423d00fddfa80437a
unknown <farefun312@gmail.com>
2022-03-18 10:44:03 +0700

    code page dashboard index

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -11,6 +11,32 @@
 	<!-- use grid row column 3 with 3 card -->
 
 	<!-- card use header and body -->
+	<div class="row">
+    	<div class="col">
+      	<div class="card" style="width: 26rem;">
+  		<div class="card-body">
+    		<h1 class="card-title">Profile</h1>
+    		<h4 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h4>
+  		</div>
+	</div>
+    	</div>
+    		<div class="col">
+      			<div class="card" style="width: 26rem;">
+  		<div class="card-body">
+    		<h1 class="card-title">Profile</h1>
+    		<h4 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h4>
+  		</div>
+	</div>
+    		</div>
+    	<div class="col">
+      		<div class="card" style="width: 26rem;">
+  		<div class="card-body">
+    		<h1 class="card-title">Profile</h1>
+    		<h4 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h4>
+  		</div>
+	</div>
+   		</div>
+  	</div>
 </div>
 
 @endsection
--- resources/views/dashboard/profile/index.blade.php
+++ resources/views/dashboard/profile/index.blade.php
@@ -1,4 +1,4 @@
-@extends('dashboard.layout.app')
+-@extends('dashboard.layout.app')
 @section('title','Dashboard - Profile')
 @section('content')
 
@@ -12,7 +12,7 @@
 	<div class="card" style="width: 26rem;">
   		<div class="card-body">
     		<h1 class="card-title">Profile</h1>
-    		<h5 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h5>
+    		<h4 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h4>
   		</div>
 	</div>
 </div>
