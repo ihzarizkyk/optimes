8a6d6dcdb5e84e62f4c5167eca3c819b5b76240f
dimasekariyan <dimaseka0024@gmail.com>
2022-03-18 07:18:33 +0700

    code page dashboard profile index

--- resources/views/dashboard/profile/index.blade.php
+++ resources/views/dashboard/profile/index.blade.php
@@ -8,6 +8,13 @@
 	<!-- using card element bootstrap 5 -->
 
 	<!-- in card using h1 and h4 element bootstrap 5 -->	
+
+	<div class="card" style="width: 26rem;">
+  		<div class="card-body">
+    		<h1 class="card-title">Profile</h1>
+    		<h5 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h5>
+  		</div>
+	</div>
 </div>
 
 @endsection
