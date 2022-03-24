5dcc1e3ba41f6e09613d34c671ee5c13d5c3ffa8
RicKo-UI <juanenricoc12@gmail.com>
2022-03-24 09:32:29 +0700

    create table in file index task (revision)

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -38,41 +38,6 @@
    		</div>
   	</div>
 	
-	<!--Tabel-->
-	<div class="row mt-5">
-		<div class="col">
-			<div class="table-responsive">
-				<table class="table table-hover">
-					<thead class="bg-dark text-white">
-						<tr>
-							<th>No</th>
-							<th>Task</th>
-							<th>Author</th>
-							<th>Status</th>
-							<th>Grade</th>
-							<th>Date</th>
-							<th>Action</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>1</td>
-							<td>Print document</td>
-							<td>Mr Agus</td>
-							<td>On Progress</td>
-							<td>70</td>
-							<td>25 Maret 2022</td>
-							<td>
-								<a href="http://" class="btn btn-warning">Edit</a>
-								<a href="http://" class="btn btn-danger">Delete</a>
-							</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		</div>
-	</div>
-	<!--End Table-->
 </div>
 
 @endsection
--- resources/views/dashboard/task/index.blade.php
+++ resources/views/dashboard/task/index.blade.php
@@ -2,11 +2,42 @@
 @section("title","Dashboard - Task Metlog")
 @section("content")
 	<div class="container">
-		
-		<!-- Buat tabel disini dengan head table (No, Name, Author, Status, Date, Action) -->
-
 		<!-- start -->
-
+		<!--Tabel-->
+		<div class="row mt-5">
+			<div class="col">
+				<div class="table-responsive">
+					<table class="table table-hover">
+						<thead class="bg-dark text-white">
+							<tr>
+								<th>No</th>
+								<th>Task</th>
+								<th>Author</th>
+								<th>Status</th>
+								<th>Grade</th>
+								<th>Date</th>
+								<th>Action</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>1</td>
+								<td>Print document</td>
+								<td>Mr Agus</td>
+								<td>On Progress</td>
+								<td>70</td>
+								<td>25 Maret 2022</td>
+								<td>
+									<a href="#" class="btn btn-warning">Edit</a>
+									<a href="#" class="btn btn-danger">Delete</a>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+		<!--End Table-->
 
 
 		<!-- end -->
