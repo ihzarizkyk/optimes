6fac5d9e159f90a095f316d1c924e085e4e8ca73
unknown <farefun312@gmail.com>
2022-03-25 10:29:06 +0700

    Create & Edit views

--- resources/views/dashboard/task/create.blade.php
+++ resources/views/dashboard/task/create.blade.php
@@ -10,15 +10,11 @@
   			<div class="card-body">
   				<form>
     				<div class="mb-3">
-    					<label for="Nama" class="form-label">Nama</label>
+    					<label for="Name" class="form-label">Name</label>
     					<input type="text" class="form-control">
     				</div>
-    				<div class="from-group">
+    				<div class="mb-3">
     					<label>Description</label>
-    					<input type="text" class="form-control">
-    				</div>
-    				<div class="from-group">
-    					<label>Author</label>
     					<input type="text" class="form-control">
     				</div>
     			</form>
--- resources/views/dashboard/task/edit.blade.php
+++ resources/views/dashboard/task/edit.blade.php
@@ -10,14 +10,14 @@
   			<div class="card-body">
   				<form>
     				<div class="mb-3">
-    					<label for="Nama" class="form-label">Nama</label>
+    					<label for="Name" class="form-label">Name</label>
     					<input type="text" class="form-control">
     				</div>
-    				<div class="from-group">
+    				<div class="mb-3">
     					<label>Description</label>
     					<input type="text" class="form-control">
     				</div>
-    				<div class="from-group">
+    				<div class="mb-3">
     					<label>Author</label>
     					<input type="text" class="form-control">
     				</div>
